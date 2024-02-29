2b8260e6f7bbfb850c1a10cd2b7268b67cab6158
Michael Selvan <michael.selvan2005@gmail.com>
2024-02-29 08:11:13 +0530

    Updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Michael"
+    author="Michael",
     author_email="michael.selvan2005@gmail.com",
     packages=find_packages()
 )
